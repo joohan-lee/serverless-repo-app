b38f014d9f062343cc235effc135c3aa99dfbf2b
Lucas <joohan0224@naver.com>
2022-04-17 19:01:44 -0700

    remove equal

--- late.js
+++ late.js
@@ -1,9 +1,10 @@
 // start coding your function here!
 function running_late(date){
     let date1 = new Date(date)
-    if (date1.getHours() <= 22){
+    if (date1.getHours() < 22){
         return "It is still early!"
-    } else{
+    }
+    else{
         return "It is late!"
     }
 }
