64716e2eed0baa44611d4451a43e8401dadd5756
Lucas <joohan0224@naver.com>
2022-04-17 18:57:33 -0700

    update late.js

--- late.js
+++ late.js
@@ -1,1 +1,11 @@
 // start coding your function here!
+function running_late(date){
+    let date1 = new Date(date)
+    if (date1.getHours() <= 22){
+        return "It is still early!"
+    } else{
+        return "It is late!"
+    }
+}
+
+module.exports = running_late;
